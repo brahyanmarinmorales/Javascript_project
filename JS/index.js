49fa8b088515a3ce22d3fb710a4ab8391231825b
Brahyan Marin <brahyan1997@hotmail.com>
2022-04-17 06:50:37 -0500

    creating the simulation for wed

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-let data=(prompt("Enter a number from 1 to 10"))
+/*let data=(prompt("Enter a number from 1 to 10"))
 
 let list
 
@@ -40,4 +40,71 @@ while(data!="ESC"){
     }
 
     data=(prompt("Continue with the sequence: "))
-}
+}*/
+
+//Simulador interativo
+
+/*let login_email= prompt("Enter your email address: ")
+let passcode= Number(prompt("Enter your password: "))
+let room_number
+
+
+const PASS1=123456
+const room_type1="Beautiful Studio"
+const room_type2="Deluxe 1BD Apartment"
+const room_type3="Luxurious 2BD Apartment"
+
+if (passcode===PASS1){
+    confirm("Welcome to the HSK report, Are you planning to create a new one")
+
+    if(confirm==true){
+        do{
+            room_number=prompt("Room Number: ")
+            console.log(room_number)
+        }
+        while (room_number!="done"){ 
+            room_number=prompt("Room Number: ")
+            console.log(room_number)
+        }   
+    }else{
+        document.write("No report was created")
+    }
+}else{
+    document.write("Password is incorrect. Try one more time")
+}*/
+
+
+let jas=prompt("Enter a number")
+
+console.log(jas.charAt(0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
