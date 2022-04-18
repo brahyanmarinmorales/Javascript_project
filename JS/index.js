0f18d251c9586e5610627050712f362ae3886cab
Brahyan Marin <brahyan1997@hotmail.com>
2022-04-17 20:49:47 -0500

    working on the simulation second stage

--- JS/index.js
+++ JS/index.js
@@ -42,42 +42,63 @@ while(data!="ESC"){
     data=(prompt("Continue with the sequence: "))
 }*/
 
+
+
+
 //Simulador interativo
 
 /*let login_email= prompt("Enter your email address: ")
 let passcode= Number(prompt("Enter your password: "))
 let room_number
 
-
-const PASS1=123456
+const PASS1=1
 const room_type1="Beautiful Studio"
 const room_type2="Deluxe 1BD Apartment"
 const room_type3="Luxurious 2BD Apartment"
 
-if (passcode===PASS1){
-    confirm("Welcome to the HSK report, Are you planning to create a new one")
 
-    if(confirm==true){
-        do{
-            room_number=prompt("Room Number: ")
-            console.log(room_number)
-        }
+if (passcode===PASS1){
+    confirm("Welcome to the HSK report, Are you planning to create a new one?")
+
+    if(true){
+
         while (room_number!="done"){ 
             room_number=prompt("Room Number: ")
             console.log(room_number)
+            room_type_verification(room_number.charAt(0))
         }   
-    }else{
-        document.write("No report was created")
     }
 }else{
     document.write("Password is incorrect. Try one more time")
+}
+
+
+function room_type_verification (room){ 
+    if(room==1){
+        room_type1
+        document.write(" " + room_number + " " + room_type1 + " ")
+    }else if(room==2){
+        room_type2
+        document.write(" " + room_number + " " + room_type2 + " ")
+    }else if(room==3){ 
+        room_type3
+        document.write(" " + room_number + " " + room_type3 + " ")
+    }
+    else{
+        document.write("This apartment does not exist")
+    }
 }*/
 
+const hsk = "Dinora";
+let type="Beautiful Studio"
+let room_number=102
 
-let jas=prompt("Enter a number")
-
-console.log(jas.charAt(0))
-
+if (type=="Beautiful Studio") {
+    for (let i = 0; i < 4; i += 1){
+        document.write(" " + hsk + " " + room_number)
+    }
+    
+}
 
 
 
